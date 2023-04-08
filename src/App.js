32c587ff32174bf6051fab1c84a6f5a9a7a3fef4
melat16 <melatkassaye13.com>
2023-04-08 10:40:30 +0300

    why choose us section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import WhyChooseUs from './components/WhyChooseUs';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
     <Home/>
-    
+    <WhyChooseUs/>
     </div>
   );
 }
---
+++ src/components/WhyChooseUs.js
@@ -0,0 +1,26 @@
+import react from 'react';
+import styled from "styled-components"
+const Container = styled.div`
+height :100vh;
+`
+const Sub = styled.div`
+height:100px;
+width:800px;
+background:yellow;
+margin-left:110px;
+border-radius:20px;
+`
+const P =styled.div`
+
+`
+export default function WhyChooseUs(){
+    return(
+
+<Container>
+    <Sub>
+
+        <P>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</P>
+    </Sub>
+</Container>
+    )
+}

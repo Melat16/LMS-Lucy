859005b29a6395d3ab5b2dbe7e400ea936746afc
Melat <melatkassaye13@gmail.com>
2023-04-09 22:05:42 +0300

    modals added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,41 @@ import React from "react";
 import styled from 'styled-components'
 import image1 from '../assets/girl.png'
 import Background from "../assets/Frame 39.png"
+import Dialog from "@material-ui/core/Dialog";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 export default function Home(){
+
+    const [open, setOpen] = React.useState(false);
+    const [opend2,setOpend2] = React.useState(false);
+    const [opend3,setOpend3] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClickOpend2 = () => {
+    setOpend2(true);
+    setOpen(false)
+  };
+
+  const handleClosed2 = () => {
+    setOpend2(false);
+  };
+  const handleClickOpend3 = () => {
+    setOpend3(true);
+    setOpend2(false)
+  };
+
+  const handleClosed3 = () => {
+    setOpend3(false);
+  };
     return(
 <Container>
 <Left>
@@ -23,7 +56,77 @@ export default function Home(){
             </Li3>
         </Ul>
     </Sub>
-    <Button>Get Started</Button>
+    <Button onClick={handleClickOpen}>Get Started</Button>
+    
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle style={{fontSize:'40px', fontWeight:'bold' , textAlign: 'center'}}>Join Now!</DialogTitle>
+        <DialogContent>
+          <DialogContentText style={{color:'rgb(96,57,147)' , fontSize:'18px', fontWeight: 'bold'}}>
+             Phone number
+          </DialogContentText>
+          <Input
+            
+            id="name"
+            label="Email Address"
+            type="email"
+           
+          />
+        </DialogContent>
+        <DialogActions>
+          <Nextbtn onClick={handleClickOpend2}> - </Nextbtn>
+        </DialogActions>
+      </Dialog>
+
+
+
+
+
+
+
+
+      <Dialog open={opend2} onClose={handleClosed2}>
+        <DialogTitle style={{fontSize:'40px', fontWeight:'bold' , textAlign: 'center'}}>Almost Done!</DialogTitle>
+        <DialogContent>
+          <DialogContentText style={{color:'rgb(96,57,147)' , fontSize:'18px', fontWeight: 'bold'}}>
+             Grade
+          </DialogContentText>
+          <Input
+            
+            id="name"
+            label="Email Address"
+            type="email"
+           
+          />
+        </DialogContent>
+        <DialogActions>
+          <Nextbtn  onClick={handleClickOpend3}> - </Nextbtn>
+        </DialogActions>
+      </Dialog>
+
+
+
+
+
+
+
+      <Dialog open={opend3} onClose={handleClosed3}>
+        <DialogTitle style={{fontSize:'40px', fontWeight:'bold' , textAlign: 'center'}}>Final Step!</DialogTitle>
+        <DialogContent>
+          <DialogContentText style={{color:'rgb(96,57,147)' , fontSize:'18px', fontWeight: 'bold'}}>
+             Experience
+          </DialogContentText>
+          <Input
+            
+            id="name"
+            label="Email Address"
+            type="email"
+           
+          />
+        </DialogContent>
+        <DialogActions>
+          <Nextbtn onClick={handleClose}> - </Nextbtn>
+        </DialogActions>
+      </Dialog>
 
 </Left>
 <Right>
@@ -60,7 +163,26 @@ height:600px;
 margin-top:10px;
 
 `
+const Input = styled.input`
+width: 480px;
+height: 40px;
+border-radius:10px;
+border-style:solid;
+border-color: rgb(96,57,147);
+border-width:0.6px;
+background-color: rgb(236, 236, 236);
 
+`
+
+const Nextbtn = styled.button`
+border-radius: 50%;
+border-style:solid;
+background-color:rgb(96,57,147);
+width: 50px;
+height: 50px;
+border-color:rgb(96,57,147);
+margin-left: 40px;
+`
 const H1 = styled.h1`
 font-size:52px;
 font-weight:400px;

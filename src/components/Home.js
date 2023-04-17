2aaf2e0678a5fd7ef480e53ee58f97e3c5f1ab70
melat16 <melatkassaye13.com>
2023-04-17 11:01:42 +0300

    add more children added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,42 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Home(){
 
     const [open, setOpen] = React.useState(false);
-    const [opend2,setOpend2] = React.useState(false);
-    const [opend3,setOpend3] = React.useState(false);
-
+    const [formFields, setFormFields] = useState([
+      { phoneNumber: '', grade: '', experience: '' },
+    ])
+  
+    const handleFormChange = (event, index) => {
+      let data = [...formFields];
+      data[index][event.target.name] = event.target.value;
+      setFormFields(data);
+    }
+  
+    const submit = (e) => {
+      e.preventDefault();
+      console.log(formFields);
+      handleClose();
+    }
+  
+    const addFields = () => {
+      let object = {
+        grade: '',
+        experience: ''
+      }
+  
+      setFormFields([...formFields, object])
+    }
+  
+    const removeFields = (index) => {
+      let data = [...formFields];
+      data.splice(index, 1)
+      setFormFields(data)
+    }
+  
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -22,22 +51,7 @@ export default function Home(){
   const handleClose = () => {
     setOpen(false);
   };
-  const handleClickOpend2 = () => {
-    setOpend2(true);
-    setOpen(false)
-  };
 
-  const handleClosed2 = () => {
-    setOpend2(false);
-  };
-  const handleClickOpend3 = () => {
-    setOpend3(true);
-    setOpend2(false)
-  };
-
-  const handleClosed3 = () => {
-    setOpend3(false);
-  };
     return(
 <Container>
 <Left>
@@ -59,74 +73,42 @@ export default function Home(){
     </Sub>
     <Button onClick={handleClickOpen}>Get Started</Button>
     
+
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle style={{fontSize:'40px', fontWeight:'bold' , textAlign: 'center'}}>Join Now!</DialogTitle>
-        <DialogContent>
-          <DialogContentText style={{color:'rgb(96,57,147)' , fontSize:'18px', fontWeight: 'bold'}}>
-             Email
-          </DialogContentText>
-          <Input
-            
-            id="name"
-            label="Email Address"
-            type="email"
-           
-          />
-        </DialogContent>
-        <DialogActions>
-          <Nextbtn onClick={handleClickOpend2}> - </Nextbtn>
-        </DialogActions>
-      </Dialog>
-
-
-
-
-
-
-
-
-      <Dialog open={opend2} onClose={handleClosed2}>
-        <DialogTitle style={{fontSize:'40px', fontWeight:'bold' , textAlign: 'center'}}>Almost Done!</DialogTitle>
-        <DialogContent>
-          <DialogContentText style={{color:'rgb(96,57,147)' , fontSize:'18px', fontWeight: 'bold'}}>
-             Grade
-          </DialogContentText>
-          <Input
-            
-            id="name"
-            label="Grade"
-            type="Number"
-           
-          />
-        </DialogContent>
-        <DialogActions>
-          <Nextbtn  onClick={handleClickOpend3}> - </Nextbtn>
-        </DialogActions>
-      </Dialog>
-
-
-
-
-
-
-
-      <Dialog open={opend3} onClose={handleClosed3}>
-        <DialogTitle style={{fontSize:'40px', fontWeight:'bold' , textAlign: 'center'}}>Final Step!</DialogTitle>
+        <DialogTitle style={{fontSize:'40px', fontWeight:'bold' , textAlign: 'center'}}>Register</DialogTitle>
         <DialogContent>
           <DialogContentText style={{color:'rgb(96,57,147)' , fontSize:'18px', fontWeight: 'bold'}}>
              Experience
           </DialogContentText>
-          <Input
-            
-            id="name"
-            label="Experience"
-            type="text"
-           
-          />
+          <form onSubmit={submit}>
+            <Input name='number' placeholder="Phone Number" />
+        {formFields.map((form, index) => {
+          return (
+            <div key={index}>
+              <Input2
+                name='grade'
+                placeholder='Grade'
+                onChange={event => handleFormChange(event, index)}
+                value={form.grade}
+              />
+              <Input2
+                name='experience'
+                placeholder='Experience'
+                onChange={event => handleFormChange(event, index)}
+                value={form.experience}
+              />
+              <Removebtn onClick={() => removeFields(index)}>-</Removebtn>
+            </div>
+          )
+        })}
+      </form>
+      <Addmorebtn onClick={addFields}>Add More Children</Addmorebtn>
+      <br />
+          
         </DialogContent>
         <DialogActions>
         <Link to='/courses'>
-        <Nextbtn > done </Nextbtn>
+        <Submitbtn onClick={submit}>Submit</Submitbtn>
                   </Link>
           
         </DialogActions>
@@ -169,24 +151,64 @@ margin-top:10px;
 
 `
 const Input = styled.input`
-width: 480px;
+width: 490px;
 height: 40px;
 border-radius:10px;
 border-style:solid;
+padding-left:10px;
 border-color: rgb(96,57,147);
 border-width:0.6px;
 background-color: rgb(236, 236, 236);
+margin-bottom:5px;
+margin-right:5px;
+
+::placeholder {
+  color:rgb(96,57,147);
+ }
+
 
 `
+const Input2 = styled.input`
+width: 235px;
+height: 40px;
+border-radius:10px;
+border-style:solid;
+padding-left:10px;
+border-color: rgb(96,57,147);
+border-width:0.6px;
+background-color: rgb(236, 236, 236);
+margin-bottom:5px;
+margin-right:5px;
+::placeholder {
+  color:rgb(96,57,147);
+ }
 
-const Nextbtn = styled.button`
+
+`
+const Submitbtn = styled.button`
+height:35px;
+width: 110px;
+background-color:rgb(96,57,147);
+border-style:solid;
+border-color:rgb(96,57,147);
+margin-left:160px;
+margin-top:10px;
+color:white;
+align-items:center;
+`
+
+const Removebtn = styled.button`
 border-radius: 50%;
 border-style:solid;
-background-color:rgb(96,57,147);
-width: 50px;
-height: 50px;
+background-color:white;
+width: 30px;
+height: 30px;
+color:red;
 border-color:rgb(96,57,147);
-margin-left: 40px;
+margin-left: 10px;
+border:solid;
+border-color:red;
+border-width:0.3px;
 `
 const H1 = styled.h1`
 font-size:52px;
@@ -210,10 +232,8 @@ font-weight:bold;
 border:none;;
 margin-right:60px;
 color:white;
-
 `
 const Sub = styled.div`
-
 border-style:solid;
 border-color:rgb(58,175,255);
 border-radius:20px;
@@ -224,7 +244,6 @@ margin-left: 250px;
 margin-bottom:40px;
 `
 const Ul = styled.ul`
-
 margin-left: 30px;
 margin-top:30px;
 `
@@ -263,4 +282,16 @@ color: rgb(248, 134, 18);
     color: rgb(58, 175, 255);
   content: "✓";
 }
+`
+
+const Addmorebtn = styled.button`
+height:35px;
+width: 150px;
+background-color:white;
+border-style:solid;
+border-color:rgb(96,57,147);
+margin-left:160px;
+margin-top:10px;
+color:rgb(96,57,147);
+align-items:center;
 `
